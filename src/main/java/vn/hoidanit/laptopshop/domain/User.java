46b61894f2d0be8e955f2c26f7222377da62b0ee
hunglegion <nvhk53c@gmail.com>
2024-07-14 17:42:54 +0700

    entity

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -1,13 +1,23 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+// @Table(name = "nguoi_dung")
 public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private long id;
     private String email;
     private String password;
     private String fullName;
     private String address;
     private String phone;
-    
+
     public long getId() {
         return id;
     }
