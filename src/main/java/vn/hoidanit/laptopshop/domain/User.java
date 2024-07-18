2a60959f28bd1af80bca5f303dea6da2365524bb
hunglegion <nvhk53c@gmail.com>
2024-07-18 07:38:21 +0700

    domain

--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -15,7 +15,14 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+    private String avatar;
 
+    public String getAvatar() {
+        return avatar;
+    }
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
     public long getId() {
         return id;
     }
@@ -55,6 +62,7 @@ public class User {
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
+    
 }
