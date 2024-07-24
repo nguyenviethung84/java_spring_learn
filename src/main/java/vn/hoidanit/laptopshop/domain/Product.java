b1d7c1221dc84c4bc720a1cf877381aab767a171
hunglegion <nvhk53c@gmail.com>
2024-07-24 07:57:29 +0700

    display product homepage

--- src/main/java/vn/hoidanit/laptopshop/domain/Product.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Product.java
@@ -1,5 +1,6 @@
 package vn.hoidanit.laptopshop.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,7 +10,6 @@ import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
 
 @Entity
 @Table(name = "products")
@@ -30,6 +30,7 @@ public class Product {
 
     @NotNull
     @NotEmpty(message = "detailDesc không để trống")
+    @Column(columnDefinition = "MEDIUMTEXT")
     private String detailDesc;
 
     @NotNull
@@ -43,67 +44,87 @@ public class Product {
     private long sold;
     private String factory;
     private String target;
-    
+
     public long getId() {
         return id;
     }
+
     public void setId(long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public double getPrice() {
         return price;
     }
+
     public void setPrice(double price) {
         this.price = price;
     }
+
     public String getImage() {
         return image;
     }
+
     public void setImage(String image) {
         this.image = image;
     }
+
     public String getDetailDesc() {
         return detailDesc;
     }
+
     public void setDetailDesc(String detailDesc) {
         this.detailDesc = detailDesc;
     }
+
     public String getShortDesc() {
         return shortDesc;
     }
+
     public void setShortDesc(String shortDesc) {
         this.shortDesc = shortDesc;
     }
+
     public long getQuantity() {
         return quantity;
     }
+
     public void setQuantity(long quantity) {
         this.quantity = quantity;
     }
+
     public long getSold() {
         return sold;
     }
+
     public void setSold(long sold) {
         this.sold = sold;
     }
+
     public String getFactory() {
         return factory;
     }
+
     public void setFactory(String factory) {
         this.factory = factory;
     }
+
     public String getTarget() {
         return target;
     }
+
     public void setTarget(String target) {
         this.target = target;
     }
+
     @Override
     public String toString() {
         return "Product [id=" + id + ", name=" + name + ", price=" + price + ", image=" + image + ", detailDesc="
